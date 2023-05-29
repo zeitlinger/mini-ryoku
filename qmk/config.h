b2bb280f9a710f6fb3d6ef5d9a7a613fb004d1f4
gregor <gregor.zeitlinger@grafana.com>
2023-05-29 13:37:35 +0200

    combos generated

--- qmk/config.h
+++ qmk/config.h
@@ -9,7 +9,12 @@
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 
-#define COMBO_TERM 100
+#define EXTRA_SHORT_COMBOS
+//#define COMBO_STRICT_TIMER
+//#define COMBO_TERM 100
+#define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
+#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
 
 // Mouse key speed and acceleration.
 #define MOUSEKEY_DELAY          0
