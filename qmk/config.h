4bc8a6b9717cc06f9949f6b31f5d12600f48b885
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-11-20 14:34:01 +0100

    aptex

--- qmk/config.h
+++ qmk/config.h
@@ -12,7 +12,7 @@
 #define EXTRA_SHORT_COMBOS
 //#define COMBO_STRICT_TIMER
 //#define COMBO_TERM 100
-#define COMBO_TERM 35        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_TERM 50        // how quickly all combo keys must be pressed in succession to trigger
 #define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
 #define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
 
