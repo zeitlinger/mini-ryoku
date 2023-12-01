8e94b64f474990ff4e9b8e148c8bffa92650b27a
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-01 21:39:10 +0100

    home row combos

--- qmk/timeout.c
+++ qmk/timeout.c
@@ -5,5 +5,33 @@
 
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
     case C_BASE_KC_K: return 80;
+     case C_BASE_LEFT_A: return 10;
+    case C_BASE_LEFT_C: return 10;
+    case C_BASE_LEFT_CA: return 10;
+    case C_BASE_LEFT_CS: return 10;
+    case C_BASE_LEFT_CSA: return 10;
+    case C_BASE_LEFT_S: return 10;
+    case C_BASE_LEFT_SA: return 10;
+    case C_BASE_RIGHT_A: return 10;
+    case C_BASE_RIGHT_C: return 10;
+    case C_BASE_RIGHT_CA: return 10;
+    case C_BASE_RIGHT_CS: return 10;
+    case C_BASE_RIGHT_CSA: return 10;
+    case C_BASE_RIGHT_S: return 10;
+    case C_BASE_RIGHT_SA: return 10;
+    case C_MODS_LEFT_A: return 10;
+    case C_MODS_LEFT_C: return 10;
+    case C_MODS_LEFT_CA: return 10;
+    case C_MODS_LEFT_CS: return 10;
+    case C_MODS_LEFT_CSA: return 10;
+    case C_MODS_LEFT_S: return 10;
+    case C_MODS_LEFT_SA: return 10;
+    case C_MODS_RIGHT_A: return 10;
+    case C_MODS_RIGHT_C: return 10;
+    case C_MODS_RIGHT_CA: return 10;
+    case C_MODS_RIGHT_CS: return 10;
+    case C_MODS_RIGHT_CSA: return 10;
+    case C_MODS_RIGHT_S: return 10;
+    case C_MODS_RIGHT_SA: return 10;
     return COMBO_TERM;
 }
