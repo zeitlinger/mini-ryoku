8322d3002f60844009f715cffd0df841b7471543
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-01 16:53:45 +0100

    home row combos

--- qmk/layout.h
+++ qmk/layout.h
@@ -26,17 +26,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
             KC_SLASH, KC_W, KC_M, KC_F, KC_NO, KC_NO, KC_Y, KC_O, KC_U, KC_MINUS,
             KC_R, KC_S, KC_T, KC_H, KC_NO, KC_NO, KC_N, KC_A, KC_I, KC_L,
-            LT(8,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(7,KC_COMMA), LT(4,KC_DOT),
+            LT(7,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(6,KC_COMMA), LT(4,KC_DOT),
                             MO(2), KC_SPC, KC_E, LT(3,KC_BSPC)),
 	[1] = LAYOUT_split_3x5_2(
             KC_SLASH, KC_W, KC_M, KC_F, KC_NO, KC_NO, KC_Y, KC_O, KC_U, KC_MINUS,
-            KC_R, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_RSFT, KC_RCTL, KC_LALT, KC_L,
-            LT(8,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(7,KC_COMMA), LT(4,KC_DOT),
+            KC_R, LALT_T(KC_S), LCTL_T(KC_T), LSFT_T(KC_H), KC_NO, KC_NO, RSFT_T(KC_N), RCTL_T(KC_A), LALT_T(KC_I), KC_L,
+            LT(7,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(6,KC_COMMA), LT(4,KC_DOT),
                             MO(2), KC_SPC, KC_E, KC_TAB),
 	[2] = LAYOUT_split_3x5_2(
             KC_SLASH, KC_W, KC_M, KC_F, KC_NO, KC_NO, KC_Y, KC_O, KC_U, KC_MINUS,
-            LT(9,KC_R), LALT_T(KC_S), LCTL_T(KC_T), LT(2,KC_H), KC_NO, KC_NO, LT(2,KC_N), RCTL_T(KC_A), LALT_T(KC_I), KC_L,
-            LT(8,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(7,KC_COMMA), LT(4,KC_DOT),
+            LT(8,KC_R), LALT_T(KC_S), LCTL_T(KC_T), LT(2,KC_H), KC_NO, KC_NO, LT(2,KC_N), RCTL_T(KC_A), LALT_T(KC_I), KC_L,
+            LT(7,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(6,KC_COMMA), LT(4,KC_DOT),
                             MO(2), KC_SPC, KC_E, LT(3,KC_BSPC)),
 	[3] = LAYOUT_split_3x5_2(
             KC_NO, KC_6, KC_5, KC_NO, KC_NO, KC_NO, KC_ESC, KC_PGUP, KC_PGDN, KC_NO,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_NO, KC_F11, KC_F12, KC_NO),
 	[5] = LAYOUT_split_3x5_2(
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_BTN1, KC_BTN2, KC_NO,
-            KC_NO, KC_NO, KC_NO, LSFT_T(KC_BTN1), KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
+            KC_NO, KC_LALT, KC_LCTL, LSFT_T(KC_BTN1), KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,
                             KC_NO, KC_NO, KC_NO, KC_NO),
 	[6] = LAYOUT_split_3x5_2(
