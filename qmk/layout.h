27b7605c40605180e0a64f72ea080226ac8e269b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-02 15:57:15 +0100

    cleanup

--- qmk/layout.h
+++ qmk/layout.h
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_SLASH, KC_W, KC_M, KC_F, KC_NO, KC_NO, KC_Y, KC_O, KC_U, KC_MINUS,
             LT(8,KC_R), LALT_T(KC_S), LCTL_T(KC_T), LSFT_T(KC_H), KC_NO, KC_NO, RSFT_T(KC_N), RCTL_T(KC_A), LALT_T(KC_I), KC_L,
             LT(7,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(6,KC_COMMA), LT(4,KC_DOT),
-                            MO(2), KC_SPC, KC_E, LT(3,KC_BSPC)),
+                            MO(2), KC_SPC, KC_E, KC_SPC),
 	[3] = LAYOUT_split_3x5_2(
             KC_NO, KC_6, KC_5, KC_NO, KC_NO, KC_NO, KC_ESC, KC_PGUP, KC_PGDN, KC_NO,
             KC_4, LALT_T(KC_3), LCTL_T(KC_2), LSFT_T(KC_1), KC_NO, KC_NO, RSFT_T(KC_LEFT), RCTL_T(KC_UP), LALT_T(KC_DOWN), KC_RIGHT,
