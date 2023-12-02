637cf1fb60592993b667a04dba6070e423375933
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-02 16:16:10 +0100

    optimize

--- qmk/custom_timeout.c
+++ qmk/custom_timeout.c
@@ -7,8 +7,6 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
   switch(index) {
     case C_BASE_KC_K: return 80;
     case C_BASE_LCTLKC_X: return 100;
-    case C_BASE_QK_ALT_REPEAT_KEY: return 20;
-    case C_BASE_QK_REPEAT_KEY: return 20;
     case C_MODS_LEFT_A: return 35;
     case C_MODS_LEFT_C: return 35;
     case C_MODS_LEFT_CA: return 35;
@@ -23,14 +21,14 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
     case C_MODS_RIGHT_CSA: return 35;
     case C_MODS_RIGHT_S: return 35;
     case C_MODS_RIGHT_SA: return 35;
-    case C_OSM_LEFT_A: return 30;
+    case C_OSM_LEFT_A: return 10;
     case C_OSM_LEFT_C: return 10;
     case C_OSM_LEFT_CA: return 30;
     case C_OSM_LEFT_CS: return 10;
     case C_OSM_LEFT_CSA: return 10;
     case C_OSM_LEFT_S: return 10;
     case C_OSM_LEFT_SA: return 10;
-    case C_OSM_RIGHT_A: return 30;
+    case C_OSM_RIGHT_A: return 10;
     case C_OSM_RIGHT_C: return 10;
     case C_OSM_RIGHT_CA: return 30;
     case C_OSM_RIGHT_CS: return 10;
