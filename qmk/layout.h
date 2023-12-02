5f740bb733f33dac3c6db17970b1398c601c43c3
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-02 11:03:43 +0100

    various

--- qmk/layout.h
+++ qmk/layout.h
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_SLASH, KC_W, KC_M, KC_F, KC_NO, KC_NO, KC_Y, KC_O, KC_U, KC_MINUS,
             KC_R, LALT_T(KC_S), LCTL_T(KC_T), LSFT_T(KC_H), KC_NO, KC_NO, RSFT_T(KC_N), RCTL_T(KC_A), LALT_T(KC_I), KC_L,
             LT(7,KC_B), KC_C, KC_G, KC_D, KC_NO, KC_NO, KC_P, RALT(KC_QUOTE), LT(6,KC_COMMA), LT(4,KC_DOT),
-                            MO(2), KC_SPC, KC_E, KC_TAB),
+                            MO(2), KC_SPC, KC_SPC, KC_TAB),
 	[2] = LAYOUT_split_3x5_2(
             KC_SLASH, KC_W, KC_M, KC_F, KC_NO, KC_NO, KC_Y, KC_O, KC_U, KC_MINUS,
             LT(8,KC_R), LALT_T(KC_S), LCTL_T(KC_T), LT(2,KC_H), KC_NO, KC_NO, LT(2,KC_N), RCTL_T(KC_A), LALT_T(KC_I), KC_L,
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_split_3x5_2(
             KC_NO, KC_6, KC_5, KC_NO, KC_NO, KC_NO, KC_ESC, KC_PGUP, KC_PGDN, KC_NO,
             KC_4, LALT_T(KC_3), LCTL_T(KC_2), LSFT_T(KC_1), KC_NO, KC_NO, RSFT_T(KC_LEFT), RCTL_T(KC_UP), LALT_T(KC_DOWN), KC_RIGHT,
-            KC_NO, KC_9, KC_8, KC_7, KC_NO, KC_NO, KC_HOME, KC_INS, KC_DEL, KC_END,
+            KC_INS, KC_9, KC_8, KC_7, KC_NO, KC_NO, KC_HOME, KC_BSPC, KC_DEL, KC_END,
                             KC_NO, KC_0, KC_NO, KC_NO),
 	[4] = LAYOUT_split_3x5_2(
             KC_NO, KC_F6, KC_F5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_NO, KC_F11, KC_F12, KC_NO),
 	[5] = LAYOUT_split_3x5_2(
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_BTN1, KC_BTN2, KC_NO,
-            KC_NO, KC_LALT, KC_LCTL, LSFT_T(KC_BTN1), KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
+            KC_NO, KC_LALT, LCTL_T(KC_BTN2), LSFT_T(KC_BTN1), KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,
                             KC_NO, KC_NO, KC_NO, KC_NO),
 	[6] = LAYOUT_split_3x5_2(
