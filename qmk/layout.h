2339941f60a74947a05d030971c6e721a84a0cde
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-05 12:38:05 +0100

    optimize mod combos

--- qmk/layout.h
+++ qmk/layout.h
@@ -31,23 +31,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             LT(1,KC_TAB), KC_SPC, KC_E, LT(2,KC_BSPC)),
 	[1] = LAYOUT_split_3x5_2(
             A(KC_F4), RCS(KC_Z), C(KC_Z), A(KC_F7), KC_NO, KC_NO, KC_INS, KC_PGUP, KC_PGDN, KC_NO,
-            CTL_T(CUT), ALT_T(COPY), SFT_T(PASTE), CTL_T(KC_TAB), KC_NO, KC_NO, CTL_T(KC_LEFT), SFT_T(KC_UP), ALT_T(KC_DOWN), KC_RIGHT,
+            CTL_T(CUT), COPY, PASTE, KC_TAB, KC_NO, KC_NO, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT,
             KC_HASH, C(KC_W), RCS(KC_V), S(KC_TAB), KC_NO, KC_NO, KC_HOME, KC_BSPC, KC_DEL, KC_END,
                             KC_NO, KC_NO, KC_TAB, KC_BSPC),
 	[2] = LAYOUT_split_3x5_2(
             KC_NO, KC_6, KC_5, ALGR(KC_CIRC), KC_NO, KC_NO, ALGR(KC_TILD), KC_UNDS, KC_PLUS, KC_NO,
-            KC_4, KC_3, KC_2, KC_1, KC_NO, KC_NO, CTL_T(ALGR(KC_DQUO)), SFT_T(KC_LCBR), ALT_T(KC_RCBR), KC_PIPE,
+            KC_4, KC_3, KC_2, KC_1, KC_NO, KC_NO, ALGR(KC_DQUO), KC_LCBR, KC_RCBR, KC_PIPE,
             KC_PERC, KC_9, KC_8, KC_7, KC_NO, KC_NO, KC_COLN, KC_LABK, KC_RABK, KC_QUES,
                             KC_SPC, KC_0, KC_NO, KC_NO),
 	[4] = LAYOUT_split_3x5_2(
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ESC, KC_BTN1, KC_BTN2, KC_BTN3,
-            MO(6), ALT_T(KC_BTN3), SFT_T(KC_BTN2), CTL_T(KC_BTN1), KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
+            MO(6), KC_BTN3, KC_BTN2, KC_BTN1, KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,
                             KC_NO, KC_NO, KC_NO, KC_NO),
 	[5] = LAYOUT_split_3x5_2(
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, ALGR(KC_GRV), KC_MINUS, KC_EQUAL, KC_NO,
             KC_NO, A(KC_F4), KC_LGUI, C(KC_W), KC_NO, KC_NO, ALGR(KC_QUOTE), KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
-            KC_NO, KC_LALT, KC_LSFT, KC_LCTL, KC_NO, KC_NO, KC_SEMICOLON, QK_ALT_REPEAT_KEY, KC_EXLM, KC_SLASH,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SEMICOLON, QK_ALT_REPEAT_KEY, KC_EXLM, KC_SLASH,
                             S(KC_TAB), KC_TAB, C(KC_Z), RCS(KC_Z)),
 	[3] = LAYOUT_split_3x5_2(
             KC_NO, KC_F6, KC_F5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
