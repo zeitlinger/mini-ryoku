1069e81e3a5885036353d0da5617cf464658c87d
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-05 08:04:45 +0100

    nav on spc+s

--- qmk/layout.h
+++ qmk/layout.h
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_PERC, KC_9, KC_8, KC_7, KC_NO, KC_NO, KC_COLN, KC_LABK, KC_RABK, KC_QUES,
                             KC_SPC, KC_0, KC_NO, KC_NO),
 	[4] = LAYOUT_split_3x5_2(
-            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_BTN1, KC_BTN2, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ESC, KC_BTN1, KC_BTN2, KC_BTN3,
             MO(6), ALT_T(KC_BTN3), SFT_T(KC_BTN2), CTL_T(KC_BTN1), KC_NO, KC_NO, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,
                             KC_NO, KC_NO, KC_NO, KC_NO),
