dc115218f7e7e33dd684d4fd7b81379ec43f6c5f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-05 11:15:36 +0100

    timeouts

--- qmk/config.h
+++ qmk/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 170
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 //#define IGNORE_MOD_TAP_INTERRUPT
