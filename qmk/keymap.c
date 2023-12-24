344567f1aa30d15eba3b21c383c701a45cb1d64e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-24 13:35:28 +0100

    basic letters on leader

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -99,9 +99,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_xcase(keycode, record)) {
         return false;
     }
-    if (!process_record_generated(keycode, record)) {
-        return false;
-    }
+//    if (!process_record_generated(keycode, record)) {
+//        return false;
+//    }
 
     switch (keycode) {
     case _HANDLER_ONE_SHOT_MOUSE:
