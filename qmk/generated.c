46b9e3671e402a4db2a81866ea34caad4753926c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-12-25 08:41:10 +0100

    basic letters on leader

--- qmk/generated.c
+++ qmk/generated.c
@@ -1,6 +1,6 @@
 /* THIS FILE WAS GENERATED!
  *
- * file is generated from https://github.com/zeitlinger/keyboard/blob/94e1da3ac87ca29749269656ce4fb67d47af9234/README.md using https://github.com/zeitlinger/keyboard/blob/94e1da3ac87ca29749269656ce4fb67d47af9234/generateKeyboard.kt
+ * file is generated from https://github.com/zeitlinger/keyboard/blob/uncommitted changes/README.md using https://github.com/zeitlinger/keyboard/blob/uncommitted changes/generateKeyboard.kt
  */
 
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
