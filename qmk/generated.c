b2371c0fc4dc0225bbd46780a674b46457ac6646
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-06-01 10:42:04 +0200

    layer key cleanup

--- qmk/generated.c
+++ qmk/generated.c
@@ -1,6 +1,6 @@
 /* THIS FILE WAS GENERATED!
  *
- * file is generated from https://github.com/zeitlinger/keyboard/blob/90f8c9cd30c1be387c609ea06713d647fb7e3280/README.md using https://github.com/zeitlinger/keyboard/blob/90f8c9cd30c1be387c609ea06713d647fb7e3280/generateKeyboard.kt
+ * file is generated from https://github.com/zeitlinger/keyboard/blob/uncommitted changes/README.md using https://github.com/zeitlinger/keyboard/blob/uncommitted changes/generateKeyboard.kt
  */
 
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
